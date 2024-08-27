78388fdcf205cfba0a4f3ac1369e9c2b037a56ac
sandeepoptiva <sandeep.singh@optiva.com>
2024-08-27 23:21:47 +0530

    Modified package name

--- HelloWorldConnector/src/main/java/com/digitizethings/sample/HelloWorldConnector.java
+++ HelloWorldConnector/src/main/java/com/digitizethings/sample/HelloWorldConnector.java
@@ -1,21 +1,4 @@
-/*
-*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
-package org.wso2.carbon.esb.connector;
+package com.digitizethings.sample;
 
 import org.apache.synapse.MessageContext;
 import org.json.simple.JSONObject;
@@ -25,7 +8,7 @@ import org.wso2.carbon.connector.core.ConnectException;
 /**
  * Sample method implementation.
  */
-public class helloWorldConnector extends AbstractConnector {
+public class HelloWorldConnector extends AbstractConnector {
 
     @Override
     public void connect(MessageContext messageContext) throws ConnectException {
@@ -38,7 +21,7 @@ public class helloWorldConnector extends AbstractConnector {
             JSONObject response = new JSONObject();
             response.put("status", responseMsg);
             log.info("response ::: " + response);
-            messageContext.setProperty("responseBody", response);
+            messageContext.setProperty("responseBody", "" + response);
         } catch (Exception e) {
             throw new ConnectException(e);
         }
